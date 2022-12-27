1d9c163962ef8ac5451916532e44b14390b88ab7
sosdan73 <daniel.sosinskiy@gmail.com>
2022-12-27 18:30:06 +0200

    Adds connection to the Star Wars API, states for loading and error

--- src/App.js
+++ src/App.js
@@ -1,34 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+    // const handleFetchMovie = async () => {
+    //     fetch('https://swapi.dev/api/films/')
+    //     .then(response => {
+    //         return response.json()
+    //     }).then(data => {
+    //         const transformedMovies = data.results.map(movie => {
+    //             return {
+    //                 id: movie.episode_id,
+    //                 title: movie.title,
+    //                 openingText: movie.opening_crawl,
+    //                 releaseDate: movie.release_date
+    //             }
+    //         })
+    //         setMovies(transformedMovies)
+    //     }).catch(err => {
+    //         console.error(err)
+    //     })
+    // }
+    const handleFetchMovie = async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch('https://swapi.dev/api/films/');
+            if (!response.ok) {
+                throw new Error('Something went wrong')
+            }
+            const data = await response.json();
+
+            const transformedMovies = data.results.map(movie => {
+                return {
+                    id: movie.episode_id,
+                    title: movie.title,
+                    openingText: movie.opening_crawl,
+                    releaseDate: movie.release_date
+                }
+            })
+            setMovies(transformedMovies);
+        } catch (error) {
+            setError(error.message)
+        }
+        setIsLoading(false);
+    }
+
+    let content = <p>Found no movies</p>;
+
+    if (!!movies.length) {
+        content = <MoviesList movies={movies} />
+    }
+    if (error) {
+        content = <p>Error</p>
+    }
+    if (isLoading) {
+        content = <p>Loading...</p>
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <button onClick={handleFetchMovie}>Fetch Movies</button>
+            </section>
+            <section>
+                {content}
+            </section>
+        </React.Fragment>
+    );
 }
 
 export default App;
